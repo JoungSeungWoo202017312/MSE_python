c90f430dec59c833f4e28b111073f1dfdc51e813
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:29:25 +0900

    added python file

---
+++ ex260.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 260
+class OMG:                    # OMG class를 생성
+    def print():              # print() 함수를 생성
+        print("Oh my god")    # "Oh my god"을 출력
+        
+        myStock = OMG()       # 
+    myStock.print()           # 
+
