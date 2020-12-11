fa408d4e4b289c8659b19e9b366f16eb16e9cc25
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:25:52 +0900

    added python file

---
+++ ex170.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 170
+result = 1                 # 1은 result
+for i in range (1,11):    # 1부터 (11-1)까지 반복
+    result *= i           # result = result * i
+print(result)             # result를 출력하라
+
