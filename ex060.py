71f7a6d0b25630818990a9e1b00047c77e6ebf2a
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:20:34 +0900

    added python file

---
+++ ex060.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 60
+nums = [1,2,3,4,5]    # [1,2,3,4,5]의 리스트는 nums
+a = sum(nums)         # a는 nums의 리스트 안의 값들의 총합
+b = a/len(nums)       # b는 a를 nums의 리스트 안의 갯수로 나눈 값
+print(b)              # b를 출력하라
+
+
+# In[ ]:
+
+
+
+
