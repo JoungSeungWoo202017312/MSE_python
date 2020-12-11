d4a7bc6dec87f7530b9dc78fc83d75f7e57a6396
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:20:09 +0900

    added python file

---
+++ ex050.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 50
+data = "039490    "     # 039490    은 data라는 변수
+print(data)             # data를 출력하라
+data = data.rstrip()    # 오른쪽 공백이 제거된 새로운 문자열 객체를 새로 data에 지정한다.
+print(data)             # data를 출력하라
+
+
+# In[ ]:
+
+
+
+
