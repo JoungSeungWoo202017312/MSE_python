c1f5799d6dd5ebd9c1bc1f02c2f8bd192e730118
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:28:10 +0900

    added python file

---
+++ ex230.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 230
+def my_print(a,b):         # a, b의 값에 따른 함수 my_print를 지정
+    print("왼쪽:", a)      # '왼쪽:'과 a의 값을 출력
+    print("오른쪽:", b)    # '오른쪽:'과 b의 값을 출력
+    
+my_print(b=100, a=200)     # a에 200, b에 100을 지정
+
