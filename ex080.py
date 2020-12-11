11d91471b3538c1b0bf94b2c7420f36904af0825
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:21:40 +0900

    added python file

---
+++ ex080.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 80
+data = tuple(range(2,100,2))    # 2부터 (100-1)까지의 값을 2의 차이로 tuple을 생성한 값은 data
+print(data)                     # data를 출력하라
+
