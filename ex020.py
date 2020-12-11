2d716eb9390bff1ce3a48058099b57aec1beae3e
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:17:07 +0900

    added python file

---
+++ ex020.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 20
+month = 48584         # 월에 48584원
+total = month * 36    # 총 금액은 36개월 동안 이자 없이 낸돈
+print(total)          # 총 금액을 출력해라.
+
