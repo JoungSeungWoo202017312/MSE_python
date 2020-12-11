fd49cd8e68387d2cc57e8db330751802cd926d34
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:26:31 +0900

    added python file

---
+++ ex190.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 190
+apart = [[101, 102], [201, 202], [301, 302]]
+for row in apart:           # apart의 리스트들을 row에 반복시켜라
+    for col in row:         # row를 col에 반복시켜라
+        print(col, "호")    # col과 '호'를 출력하라
+print("-" * 5)              # -를 5번 출력하라
+
