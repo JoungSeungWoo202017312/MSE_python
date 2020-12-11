50a49aec4012ebe5720e121f6da55f6c2a72a702
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:21:19 +0900

    added python file

---
+++ ex070.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 70
+data = [2,4,3,1,5,10,9]    # 이 리스트는 data
+b = sorted(data)           # b는 data의 안의 값들을 오름차순으로 정리한 값
+print(b)                   # b를 출력하라
+
