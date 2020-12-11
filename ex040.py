bf0d989ef581caf825f1dff3599913ec3fa5867c
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:19:12 +0900

    added python file

---
+++ ex040.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 40
+data = "    삼성전자    "    #     삼성전자    라는 문자열을 data의 변수에 지정
+print(data)                  # data를 출력하라
+a = data.strip()             # data에서 좌우 공백을 제거한 값은 a
+print(a)                     # a를 출력하라
+
+
+# In[ ]:
+
+
+
+
