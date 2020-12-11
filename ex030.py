683af14cfb835585e555f6b5ddf19a484de38c52
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:18:38 +0900

    added python file

---
+++ ex030.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 30 
+string = 'abcd'            # abcd라는 문자열을 string의 변수에 지정
+string.replace('b','B')    # string의 변수에서 b를 B로 대체하라
+print(string)              # string을 출력하라
+# 문자열은 변경할 수 없는 자료형이므로 출력값은 aBcd가 아닌 abcd로 나온다.
+
+
+# In[ ]:
+
+
+
+
