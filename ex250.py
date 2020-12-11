7e4017e4399ca63b1e6fc320b716a5b89c9395c4
JoungSeungWoo <sossosv@naver.com>
2020-12-11 19:29:01 +0900

    added python file

---
+++ ex250.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 250
+import numpy                         # numpy를 사용하기 위해
+for i in numpy.arange(0, 5, 0.1):    # 0부터 5까지 0.1씩 증가하도록 루프를 생성
+    print(i)                         # i를 출력
+
